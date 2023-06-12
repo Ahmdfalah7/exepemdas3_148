a711af3656f06c78554e548b936dc9694c3ca254
falskuyyy <ahmdfalah10@gmail.com>
2023-06-12 10:29:44 +0700

    selesai

--- ucp2/ucp2.cpp
+++ ucp2/ucp2.cpp
@@ -8,7 +8,7 @@ public:
 	bidangDatar() {	//constructor
 		x = 0;
 	}
-	virtual void input() {} // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+	virtual void input() {} // fungsi yang menerima input dan mengirim input melalui fungsi set
 		virtual float Luas(int a) { return 0; } //fungsi untuk menghitung luas
 	virtual float Keliling(int a) { return 0; } //fungsi untuk menghitung keliling
 	void setX(int a) { //fungsi untuk memberi/mengirim nilai pada x
@@ -18,8 +18,60 @@ public:
 		return x;
 	}
 };
-class Lingkaran :public bidangDatar { /*lengkapi disini*/ };
-class Bujursangkar :public bidangDatar { /*lengkapi disini*/ };
-int main() { /*lengkapi disini*/ }
+class Lingkaran : public bidangDatar {
+public:
+	void input() {
+		int r;
+		cout << "Masukkan jejari: ";
+		cin >> r;
+		setX(r);
+	}
 
+	float Luas(int a) {
+		return 3.14 * a * a;
+	}
+
+	float Keliling(int a) {
+		return 2 * 3.14 * a;
+	}
+};
+
+class Bujursangkar : public bidangDatar {
+public:
+	void input() {
+		int sisi;
+		cout << "Masukkan sisi: ";
+		cin >> sisi;
+		setX(sisi);
+	}
+
+	float Luas(int a) {
+		return a * a;
+	}
+
+	float Keliling(int a) {
+		return 4 * a;
+	}
+};
+
+int main() {
+	bidangDatar* objek;
+
+	//lingkaran
+	cout << "Lingkaran dibuat" << endl;
+	objek = new Lingkaran();
+	objek->input();
+	cout << "Luas Lingkaran = " << objek->Luas(objek->getX()) << endl;
+	cout << "Keliling Lingkaran = " << objek->Keliling(objek->getX()) << endl;
+
+	// Bujursangkar
+	cout << "Bujursangkar dibuat" << endl;
+	objek = new Bujursangkar();
+	objek->input();
+	cout << "Luas Bujursangkar = " << objek->Luas(objek->getX()) << endl;
+	cout << "Keliling Bujursangkar = " << objek->Keliling(objek->getX()) << endl;
+
+	delete objek;
+
+	return 0;
 };
